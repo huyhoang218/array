7dfc091b0d508f06d6dee66b3a529a8ebe7dfcb6
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-04-17 15:53:44 +0700

    ex01

--- exArray/ex01.js
+++ exArray/ex01.js
@@ -90,5 +90,46 @@ function enterArray(num, arr) {
 //Bài 6- Viết chương trình khởi tạo/nhập vào một mảng số nguyên gồm 10 phần tử, nhập/tạo phần tử số nguyên V.
 // Chương trình tìm xem V có nằm trong mảng số nguyên không? Nếu V thuộc mảng số nguyên thì in ra "V is in the array" còn lại in ra "V is not in the array".
 
+// let num, arr = []
+// enterArray(num, arr)
+// let intV
+// do {
+//     intV = +prompt('Nhap so nguyen V')
+// }while (!checkInt(intV))
+// let checkIntV = false
+// let count = 0
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === intV) {
+//         checkIntV = true
+//         //bai7
+//         // arr.splice(i,1)
+//         for (let j = i; j < arr.length; j++) {
+//           arr[j] = arr[j+1]
+//         }
+//         arr[arr.length - 1] = 0
+//     }
+// }
+//
+// if(checkIntV === true) console.log('V is in the array')
+// else console.log('V is not in the array')
+// console.log(arr)
+
+//Bài 8- Viết chương trình khởi tạo/nhập vào một mảng số nguyên gồm 10 phần tử. Chương trình sắp xếp mảng theo thứ tự giảm dần và hiển thị ra mảng đã được sắp xếp.
+
 let num, arr = []
 enterArray(num, arr)
+let len = arr.length
+let max = 0
+for (let i = 0; i < len - 1; i++) {
+    max = i
+    for (let j = i + 1; j < len; j++) {
+        if(arr[j] > arr[max]){
+            max = j
+        }
+    }
+    let temp = arr[i]
+    arr[i] = arr[max]
+    arr[max] = temp
+}
+console.log(arr)
+//Bai 9 tuong tu
